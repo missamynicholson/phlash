e3304aa9f1f036071f97155578614ab25d67097a
Amy Nicholson <missamynicholson@gmail.com>
2016-06-15 10:03:29 +0100

    Initial Commit

---
+++ Phlash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Phlash
+//
+//  Created by Amy Nicholson on 15/06/2016.
+//  Copyright © 2016 Phlashers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
